5c37014490ff8e682785c9ccf6634d353ffc1d32
ChidiUmeh <137958234+ChidiUmeh@users.noreply.github.com>
2023-09-08 08:46:03 +0100

    changes made

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000
 
 
 let weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-const details = {
+const details = [{
     slack_name:"ChidiebereAgate",
     current_day:weekdays[new Date().getUTCDay()],
     utc_time:moment.utc(),
@@ -19,18 +19,18 @@ const details = {
     github_file_url:"https://github.com/ChidiUmeh/Queryendpoint/blob/main/app.js",
     github_repo_url:"https://github.com/ChidiUmeh/Queryendpoint",
     status_code : 200
-}
+}]
 
 
 
 
 app.get('/api', (req,res)=>{
     const {slack_name,track} = req.query
-//    const checkQuery = details.find(name=>name.slack_name===slack_name && name.track===track)
-//    console.log(checkQuery.utc_time)
-//    if(checkQuery)
-   return res.status(200).json(details)
-//    return res.status(404).json({error: "does not exist"})
+   const checkQuery = details.find(name=>name.slack_name===slack_name && name.track===track)
+   console.log(checkQuery.utc_time)
+   if(checkQuery)
+   return res.status(200).json(checkQuery)
+   return res.status(404).json({error: "does not exist"})
 })
 
 
