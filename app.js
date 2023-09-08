244da24fdf11048cd1490069757f794b44ed99ed
ChidiUmeh <137958234+ChidiUmeh@users.noreply.github.com>
2023-09-08 10:51:55 +0100

    changes made

--- app.js
+++ app.js
@@ -10,30 +10,30 @@ app.use(nocache())
 
 
 let port = process.env.PORT || 3000
-let currentDate = new Date();
+
+
+
+
+
+app.get('/api', (req,res)=>{
+    let currentDate = new Date();
 let formattedUTCDate = currentDate.toISOString().slice(0, -5) + 'Z';
 
 let weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 let details = [{
     slack_name:"ChidiebereAgate",
     current_day:weekdays[new Date().getUTCDay()],
-    utc_time:new Date(),
+    utc_time:formattedUTCDate,
     track:"backend",
     github_file_url:"https://github.com/ChidiUmeh/Queryendpoint/blob/main/app.js",
     github_repo_url:"https://github.com/ChidiUmeh/Queryendpoint",
     status_code : 200
 }]
-
-
-
-
-app.get('/api', (req,res)=>{
     const {slack_name,track} = req.query
    const checkQuery = details.find(name=>name.slack_name===slack_name && name.track===track)
-   if(checkQuery){
-    checkQuery.utc_time = formattedUTCDate
-   }
+   if(checkQuery)
    return res.status(200).json(checkQuery)
+   return res.status(404).json({error: "does not exist"})
 })
 
 
--- index.js
+++
@@ -1,6 +0,0 @@
-const currentDate = new Date();
-const formattedUTCDate = currentDate.toISOString().slice(0, -5) + 'Z';
-
-console.log(formattedUTCDate)
-
-
