5de1d25c7c9083e38a7653ca03a7afb73aa4f7d1
ChidiUmeh <137958234+ChidiUmeh@users.noreply.github.com>
2023-09-08 01:57:27 +0100

    changes made

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors())
 const port = process.env.PORT || 3000
 
 
-let weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 let date = new Date().toISOString().split('')
 date.splice(-5,4)
 let utc_date = date.join("")
